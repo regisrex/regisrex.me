50bf2bac48313b898ef97d730b314dfc8e42e861
rex <ndizihiweregis06@gmail.com>
2024-12-08 21:58:59 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     
+    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center items-center   min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <span className="text-2xl font-black">R&eacute;gis N. <span className="opacity-60">(regisrex)</span></span> <br />
     </div>
   );
 }
